3ae89ddb2f74b58b0094f50fa18513be31291054
CashlockH <ilyashesenli48@gmail.com>
2024-03-31 22:18:44 +0400

    else condition is added for no converter inputs

--- printf.c
+++ printf.c
@@ -102,9 +102,12 @@ int _printf(const char *format, ...)
 			{
 				write(1, &format[j], 1);
 			}
+			else
+				write(1, "%", 1), write(1, &format[j], 1);
 		}
 		else
 			write(1, &format[j], 1);
 		j++;
+	}
 	va_end(ap);
 }
