f132c7795ee5fffbaedd63e123745f2502e468e1
RubyHwaryun <abiyev.rasul44@gmail.com>
2024-03-31 09:10:15 +0000

    Header added to printf

--- printf.c
+++ printf.c
@@ -1,3 +1,7 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 /**
  * _digit - prints the digits of the number as a character one by one
  * @a: the number should be printed
